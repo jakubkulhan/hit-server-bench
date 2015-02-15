d39c9d1ec4344291749663f1c40749b2c29b19ab
Changlin Li <mail@changlinli.com>
2015-02-15 16:05:15 -0500

    New Haskell Warp benchmark
    The old benchmark wouldn't compile, did not declare its dependencies,
    had some odd compiler flags, and did not tell GHC to compile as a
    multithreaded program.
    
    To fix these, this commit adds
    
    + Packaging this entire thing as a single cabal package which takes care
      of all the dependency management.
    + Adding additional/fixing existing compiler flags
        * -O3 does not exist; I've replaced it with -O2
        * Added an additional -threaded flag to tell GHC to compile this
          with multithreaded support
    + When running the program, we add an "+RTS -N" to tell the runtime
      system to run it on all available cores
    
    The result of these changes is that on my dual-core five-year-old laptop
    with an Intel Core i5-450M, Warp is three times faster than Node.

--- warp/warped.hs
+++ warp/warped.hs
@@ -7,10 +7,11 @@ import Network.Wai.Handler.Warp (run)
 import qualified Data.ByteString.Lazy.Char8 as LB8
 import Data.UnixTime (getUnixTime, utSeconds)
 
-application _ = do
+application2 :: Application
+application2 _ respond = do
   time <- getUnixTime
   let seconds = utSeconds time
-  return $ responseLBS status200 [("Content-Type", "text/plain")] $ LB8.pack (show seconds)
+  respond $ responseLBS status200 [("Content-Type", "text/plain")] $ LB8.pack (show seconds)
 
 main :: IO ()
-main = run 8080 application
+main = run 8080 application2
